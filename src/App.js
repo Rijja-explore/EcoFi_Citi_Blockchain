e2ba69ff30e8f48d17ae9b8aa521e95293c33e3d
Rijja H <rijja.softwaredev@gmail.com>
2025-09-15 23:56:53 +0530

    The Ultimate UI

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import MergedEcoFiDashboard from './MergedEcoFiDashboard';
-import Onboarding from './components/Onboarding';
-import PerformanceOptimizer from './components/PerformanceOptimizer';
+import React from 'react';
+import UltimateEcoFiDashboard from './UltimateEcoFiDashboard';
 import './App.css';
 
 function App() {
-  const [showOnboarding, setShowOnboarding] = useState(false);
-  
-  useEffect(() => {
-    // Check if this is first time use
-    const hasVisitedBefore = localStorage.getItem('ecofi_hasVisitedBefore');
-    if (!hasVisitedBefore) {
-      setShowOnboarding(true);
-      localStorage.setItem('ecofi_hasVisitedBefore', 'true');
-    }
-  }, []);
-
   return (
     <div className="App">
-      <MergedEcoFiDashboard />
-      {showOnboarding && (
-        <Onboarding 
-          isOpen={showOnboarding} 
-          onComplete={() => setShowOnboarding(false)} 
-        />
-      )}
-      <PerformanceOptimizer />
+      <UltimateEcoFiDashboard />
     </div>
   );
 }
